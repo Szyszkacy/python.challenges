3443177629c127bf11db30b37a001f76dde8e432
Szyszkacy <110200338+Szyszkacy@users.noreply.github.com>
2023-02-18 20:08:53 +0100

    Add files via upload

---
+++ 6.2 Write your own function.py
@@ -0,0 +1,13 @@
+#ex1
+def cube(x):
+    product = x**3
+    return product
+
+print(f'0 cubed is {cube(0)}')
+print(f'2 cubed is {cube(2)}')
+
+
+#ex2
+def greet(name):
+    print(f"Hello, {name}!")
+greet('Kuba')
