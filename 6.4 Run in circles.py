c212f5b24e6f89d795dd423262d64fa64e5fceee
Szyszkacy <110200338+Szyszkacy@users.noreply.github.com>
2023-02-19 18:58:18 +0100

    Add files via upload

---
+++ 6.4 Run in circles.py
@@ -0,0 +1,21 @@
+#ex1
+for num in range(2,11):
+    print(num)
+#ex2
+number = 2
+while number <= 10:
+    print(number)
+    number +=1
+    
+#ex3
+def doubles(num):
+    return num * 2
+
+number1 = 2
+for n in range(0,3):
+    number1 = doubles(number1)
+    print(number1)
+    
+
+    
+
